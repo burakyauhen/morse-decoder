9ba484209e94df828285c83e5702aefea8f79936
Burak Yauhen <burakyauhen@gmail.com>
2023-02-10 21:49:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,45 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    
+    function getArrDate(expr) {
+        let arr = [];
+        while (expr) {
+            arr.push(expr.slice(0, 10));
+            expr = expr.slice(10);
+        }
+        return arr;
+    }
+    
+    function decodeArr(arr) {
+        arr = arr.map(function(item) {
+            if (item === '**********') return ' ';
+            while (item[0] === '0') item = item.slice(1);
+                let arr1 = [];
+            while (item) {
+                let str = item.slice(0, 2);
+                item = item.slice(2);
+                if (str === '10') arr1.push('.');
+                if (str === '11') arr1.push('-')
+            }
+        return arr1.join('');
+        });
+        return arr; 
+    }
+    
+    function getText(arr) {
+        arr = arr.map(function(item) {
+            if (item === ' ') return ' ';
+            return MORSE_TABLE[item]; 
+        });
+        return arr;
+    }
+    
+    let arr = getArrDate(expr);
+    arr = decodeArr(arr);
+    arr = getText(arr);
+    
+    return arr.join('');
 }
 
 module.exports = {
